bdd20bf1bc74f74e0934571352a65ae51c312b0c
Hemtros <hemtros@gmail.com>
2013-11-26 15:42:47 +0545

    initial commit

---
+++ aboutus.php
@@ -0,0 +1,40 @@
+<html>
+<head><title></title>
+<link href="styles/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="styles/main.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="scripts/bootstrap.js"> </script>
+<script type="text/javascript" src="scripts/jquery-1.10.2.min.js"></script>
+</head>
+
+<body>
+
+<div id="banner">
+	
+	<a href="index.php"><img src="images/logo.png" /></a><span id="websiteheading">Product Monitoring Service</span>
+
+	<span id="tagline">(Mining Social media to monitor your product)</span>
+
+</div>
+
+<div id="navbar">
+	
+	<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="#">Monitor</a></li>
+	<li><a href="#">Support</a></li>
+	<li><a href="aboutus.php">About Us</a></li>
+	</ul>
+</div>
+
+<div id="content">
+
+	<h1>About us</h1>
+
+</div>
+
+<div id="footer">
+	<p>&copy;&nbsp;Copyright 2013</p>
+</div>
+
+</body>
+</html>
---
+++ login_page.php
@@ -0,0 +1,57 @@
+<html>
+<head><title></title>
+<link href="styles/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="styles/main.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript" src="scripts/bootstrap.js"> </script>
+<script type="text/javascript" src="scripts/jquery-1.10.2.min.js"></script>
+
+</head>
+
+<body>
+
+<div id="banner">
+	<a href="index.php"><img src="images/logo.png" /></a><span id="websiteheading">Product Monitoring Service</span>
+
+	<span id="tagline">(Mining Social Media to monitor your product)</span>
+</div>
+
+<div id="navbar">
+	<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="#">Monitor</a></li>
+	<li><a href="#">Support</a></li>
+	<li><a href="aboutus.php">About Us</a></li>
+	</ul>
+</div>
+
+<div id="content">
+
+<div id="sidebar">
+	<ul>
+		<br /><br />
+		<li><a href="registration_page.php">Register</a></li>
+		<li><a href="login_page.php">Login</a></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>  <!-- end of sidebar   -->
+
+
+	
+	
+  
+
+
+	
+</div>
+
+<div id="footer">
+	
+	<p>&copy;&nbsp;Copyright 2013</p>
+</div>
+
+
+
+</body>
+</html>
